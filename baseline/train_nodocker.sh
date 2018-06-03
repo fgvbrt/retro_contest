878691688e80707c00ca9547ae4b67c2e21f0459
mpavlov <fgvbrt123@gmail.com>
2018-06-03 15:14:35 +0300

    choose card in local training

--- baseline/train_nodocker.sh
+++ baseline/train_nodocker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_DEVICE_ORDER="PCI_BUS_ID" CUDA_VISIBLE_DEVICES="4" python ppo2_agent.py --config config.yaml config_train.yaml
+CUDA_DEVICE_ORDER="PCI_BUS_ID" CUDA_VISIBLE_DEVICES="$1" python ppo2_agent.py --config config.yaml config_train.yaml
