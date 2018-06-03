9f3ad7fb564b9bd8e4bc6d10a1a1513e4125e921
mpavlov <fgvbrt123@gmail.com>
2018-06-03 15:12:44 +0300

    fix setup.sh script

--- baseline/setup.sh
+++ baseline/setup.sh
@@ -3,7 +3,7 @@ conda create -n retro python=3.5 -y
 source activate retro
 pip install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp35-cp35m-linux_x86_64.whl
 pip install -r requirements.txt
-pip install --no-deps git+https://github.com/fgvbrt/baselines.git@1659068fdeb5fd4859fa598634008a84afe3616e
+pip install --no-deps git+https://github.com/fgvbrt/baselines.git@bf47fe6b6237825fb20aa72551795bc3680be969
 git clone https://github.com/openai/retro-contest.git && cd retro-contest/support && pip install .
 
 # download roms
